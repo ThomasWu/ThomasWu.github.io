91f369b351e875301f90931429c5a64d59147a27
Chuanzhen Wu <kavfwu@udel.edu>
2016-03-08 16:42:04 -0500

    countdown

--- countdown.js
+++ countdown.js
@@ -1,14 +1,16 @@
-function CountDown($scope, $timeout) {
-      var time = 10;
-      countDowner = function() {
-        if (time < 0) {
-          $("#warning").fadeOut(2000);
-          time = 0;
-          return; // quit
-        } else { // update scope
-          countDown--; // -1
-          $timeout(countDowner, 1000); // loop it again
+angular.module('todoApp', [])
+  .controller('CountDownController', function() {
+    var cd = this;
+    cd.time = 5;
+    
+    countdown = function($scope, $timeout) {
+        if (cd.time<0) {
+            cd.time = 0;
+            return;
         }
-      };
-      countDowner()
-}
+        cd.time--;
+        $timeout(countdown, 1000);
+    }
+    
+    countdown()
+})
